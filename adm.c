115243425028039dff8534e0433e0a583bb9c7ed
viniciuscubi <viniciuscubi@gmail.com>
2020-06-21 04:30:33 -0300

    < função excluir post com o menu corrigido mas ainda infuncional, erro de segmentação>

--- adm.c
+++ adm.c
@@ -15,6 +15,8 @@ printf("[3] Logout\n");
 int verif;
 get_int(&verif);
 
+char excluir_bin = ' ' ;
+int excluir_numero = 0;
 FILE *fp;
 fp = fopen("adm", "a+");
 
@@ -37,57 +39,47 @@ switch (verif) {
 		int flag=1;
 		int linha=0;
 		int num=0; //numero de posts
-		while(fgets(teste , 128 , fp)==NULL) {
+		while(fgets(teste , 128 , fp)!=NULL) {
 			num++;
 		}
 		printf("Existem atualmente %d posts na timeline geral\n", num);
 		fseek(fp, 0, SEEK_SET);
-		while(flag) {
-		for(int i=1; i<=5; i++) {
-			if(fgets(teste, 128, fp)==NULL) {//alt
-				break;
+		    for(int linha=1; linha<=num; linha++) {
+			    fgets(posts, 128, fp);
+			    printf("%d %s\n", linha, posts);
 			}
-			else {
-				linha++;
-				for(int j=0; j<128; j++) {
-					fgets(posts, 200, fp); //  {
-						printf("%d %s\n", linha, posts);
-						//}
-					}
-				}
-		}
-		printf("\nDeseja excluir o post (i)? Exemplo: 1 [s/n]\n");
-                                                int excluir_numero;
-                                                char excluir_bin;
-                                                get_int(&excluir_numero);
-                                                while(excluir_numero<1 ||  excluir_numero>num) {
-                                                        printf("Resposta invalida\n");
-                                                        get_int(&excluir_numero);
-                                                }
-                                                get_char( &excluir_bin);
-                                                while(excluir_bin =! 's' && excluir_bin!='n' ) {
-                                                        printf("Resposta invalida/n");
-                                                        get_char( &excluir_bin);
-                                                                 }
-                                                if(excluir_bin =='s'){
-                                                        apagar_post(fp, excluir_numero, num);
-                                                }
+		    printf("\nDeseja excluir algum post? [s/n]\n");
+            get_char(&excluir_bin);
+            while(excluir_bin != 's' && excluir_bin!='n' ) {
+                printf("Resposta invalida/n");
+                get_char(&excluir_bin);
+            }
+            if(excluir_bin == 's'){
+                printf("\nQual post gostaria de escolher escolha um numero de 1 à %d\n",num);
+                get_int(&excluir_numero);
+                while(excluir_numero<1 ||  excluir_numero>num) {
+                    printf("Resposta invalida\n");
+                    get_int(&excluir_numero);
+                }
+                apagar_post(fp, excluir_numero, num,"adm");
+             }
 
-		printf("Deseja ver mais postagens? [s/n]");
-		char resposta;
-		get_char(&resposta);
-		while(resposta != 's' && resposta!= 'n') {
-			printf("Resposta invalida\n");
-			get_char(&resposta);
-			}	
-		if(resposta=='n'){
-			flag=0;
-			}
-		}
-        free(posts);
-        free(teste);
-        break;
+		    printf("Deseja ver mais postagens? [s/n]");
+		    char resposta;
+		    get_char(&resposta);
+		    while(resposta != 's' && resposta!= 'n') {
+			    printf("Resposta invalida\n");
+			    get_char(&resposta);
+		    }	
+		    if(resposta=='n'){
+			    flag=0;
+		    }
+	
+    free(posts);
+    free(teste);
+    break;
     }
+
 	case 2:{
 	char *aux;
     int	num=0;
